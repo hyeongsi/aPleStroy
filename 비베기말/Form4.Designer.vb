6847ad1d32b2dab37f6bdb1e4bc4b0736b903d43
parkSihyeong46 <71202869+parkSihyeong46@users.noreply.github.com>
2020-11-28 20:28:14 +0900

    스토리추가

--- 비베기말/Form4.Designer.vb
+++ 비베기말/Form4.Designer.vb
@@ -22,17 +22,53 @@ Partial Class Form4
     '코드 편집기에서는 수정하지 마세요.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("맑은 고딕", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.Label1.Location = New System.Drawing.Point(357, 53)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 21)
+        Me.Label1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.BackgroundImage = Global.비베기말.My.Resources.Resources.button11
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("맑은 고딕", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button1.Location = New System.Drawing.Point(710, 53)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(62, 66)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "나가기"
+        Me.Button1.UseVisualStyleBackColor = False
         '
         'Form4
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(784, 411)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
         Me.DoubleBuffered = True
         Me.Name = "Form4"
         Me.Text = "ApleStory"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
 End Class
--- 비베기말/Form4.vb
+++ 비베기말/Form4.vb
@@ -3,6 +3,17 @@ Public Class Form4
     Private Declare Function GetTickCount64 Lib "kernel32" () As Long
     Private Declare Function GetAsyncKeyState Lib "user32" (ByVal vkey As Integer) As Short
 
+    Public Declare Function mciSendString Lib "winmm.dll" Alias "mciSendStringA" _
+    (ByVal lpstrCommand As String, ByVal lpstrReturnString As String, ByVal uReturnLength As _
+     Integer, ByVal hwndCallback As Integer) As Integer
+    Dim brush1 As SolidBrush
+    Dim brush2 As SolidBrush
+    Dim timeGauge As Integer
+
+    Dim isStart As Boolean = False
+    Dim isEnd As Boolean = False
+
+    Dim startTime As Long
     Dim currentTime As Long
     Dim lastTime As Long
     Dim lastPlayerAnimTime As Long
@@ -14,7 +25,10 @@ Public Class Form4
     Dim ldBitmap As Bitmap
 
     Dim playerImage As Image
-    Dim playerBitmap(4) As Bitmap
+    Dim playerBitmap(5) As Bitmap
+
+    Dim monsterImage As Image
+    Dim monsterBitmap As Bitmap
 
     Dim thread_main As Thread
     Structure Rect
@@ -38,13 +52,16 @@ Public Class Form4
     End Structure
 
     Dim plrInfo As CharInfo
+    Dim isJump As Boolean
+    Dim isJumpEffectSound As Boolean
+
+    Dim velocity As Integer = 28
 
     Private Sub Form4_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         thread_main = New Thread(AddressOf Main) With {.IsBackground = True}
 
-        currentTime = GetTickCount64()
-        lastTime = GetTickCount64()
-        lastPlayerAnimTime = GetTickCount64()
+        My.Computer.Audio.Play("sound\hanggu.wav", AudioPlayMode.BackgroundLoop)
+        mciSendString("open sound\jump.wav alias jumpsound1", 0, 0, 0)
     End Sub
     Private Sub Form4_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
         thread_main.Start()
@@ -70,6 +87,12 @@ Public Class Form4
         ldRect3.y = 450 - 75
         ldRect3.width = 665
         ldRect3.height = 59
+
+        brush1 = New SolidBrush(Color.Black)
+        brush2 = New SolidBrush(Color.Red)
+
+        timeGauge = 0
+        isStart = True
     End Sub
     Sub SpawnPlayer()
         plrInfo.hp = 3
@@ -77,6 +100,9 @@ Public Class Form4
         plrInfo.pos.y = 287
         plrInfo.anim = 0
         plrInfo.state = 0
+
+        isJump = False
+        isJumpEffectSound = False
     End Sub
     Sub LoadBitmap()
         backImage = My.Resources.ResourceManager.GetObject("backImage")
@@ -86,6 +112,10 @@ Public Class Form4
         ldImage = My.Resources.ResourceManager.GetObject("loadimage")
         ldBitmap = New Bitmap(ldImage)
         ldBitmap.MakeTransparent()
+
+        monsterImage = My.Resources.ResourceManager.GetObject("junior_lace")
+        monsterBitmap = New Bitmap(monsterImage)
+        monsterBitmap.MakeTransparent()
 
         playerImage = My.Resources.ResourceManager.GetObject("Char_9_move")
         playerBitmap(0) = New Bitmap(playerImage)
@@ -102,9 +132,21 @@ Public Class Form4
         playerImage = My.Resources.ResourceManager.GetObject("Char_12_move")
         playerBitmap(3) = New Bitmap(playerImage)
         playerBitmap(3).MakeTransparent()
+
+        playerImage = My.Resources.ResourceManager.GetObject("Char_28_jump_move")
+        playerBitmap(4) = New Bitmap(playerImage)
+        playerBitmap(4).MakeTransparent()
     End Sub
 
     Private Sub Main()
+        MsgBox("학업을 위해 옆 동네 도서관에 간 사이에" & vbCrLf & vbCrLf & "마을이 몬스터에 의해 공격받고 있다는 소식을 들었습니다." & vbCrLf & vbCrLf &
+            "이대로 가다간 마을 주민들이 몬스터들에게 당해" & vbCrLf & vbCrLf & "코딩 노예가 될 것 입니다." & vbCrLf & vbCrLf & "가는 길에 마주치는 몬스터를 상대할 시간도 없습니다." & vbCrLf & vbCrLf &
+            "몬스터들을 피해 빨리 마을로 달려가야겠습니다.",, "스토리")
+
+        startTime = GetTickCount64()
+        currentTime = GetTickCount64()
+        lastTime = GetTickCount64()
+        lastPlayerAnimTime = GetTickCount64()
 
         Do
             currentTime = GetTickCount64()
@@ -115,8 +157,15 @@ Public Class Form4
                 SetState()
                 SwichPlayerAnim()
 
+                MonsterSpawn()
+                MonsterMove()
+
                 Invoke(Sub() Me.Invalidate())
+
+                Jump()
+
             End If
+
         Loop
 
     End Sub
@@ -147,7 +196,11 @@ Public Class Form4
         End If
     End Sub
     Sub SetState()
-
+        If GetAsyncKeyState(Keys.Menu) And isJump = False Then    'alt key input
+            isJumpEffectSound = True
+            plrInfo.state = 1
+            isJump = True
+        End If
     End Sub
     Sub SwichPlayerAnim()
         If plrInfo.state = 0 Then
@@ -163,9 +216,35 @@ Public Class Form4
             Else                                'left idle init
                 plrInfo.anim = 0
             End If
+        ElseIf plrInfo.state = 1 Then       'jump
+            plrInfo.anim = 4
         End If
     End Sub
+    Sub Jump()
+        If isJump = True Then
+            plrInfo.pos.y -= velocity
+            velocity -= 4
+
+            If plrInfo.pos.y >= 287 Then
+                velocity = 28
+                isJump = False
+                plrInfo.state = 0
+            End If
+        End If
+
+    End Sub
+
+    Sub MonsterSpawn()
+
+    End Sub
+
+    Sub MonsterMove()
+
+    End Sub
+
     Private Sub Form4_Paint(sender As Object, e As PaintEventArgs) Handles MyBase.Paint
+        Label1.Text = CInt((currentTime - startTime) / 1000) & "초"
+
         e.Graphics.DrawImage(backBitmap, backMap.x, 0, backBitmap.Width, backBitmap.Height)
         e.Graphics.DrawImage(backBitmap, backMap2.x, 0, backBitmap.Width, backBitmap.Height)
 
@@ -173,9 +252,50 @@ Public Class Form4
         e.Graphics.DrawImage(ldBitmap, ldRect2.x, ldRect2.y, ldBitmap.Width, ldBitmap.Height)
         e.Graphics.DrawImage(ldBitmap, ldRect3.x, ldRect3.y, ldBitmap.Width, ldBitmap.Height)
 
-        e.Graphics.DrawImage(playerBitmap(plrInfo.anim), plrInfo.pos.x, plrInfo.pos.y, playerBitmap(plrInfo.anim).Width, playerBitmap(plrInfo.anim).Height)
+        If timeGauge >= Me.Width - 70 Then
+            plrInfo.pos.x += 3
+        End If
+
+        If plrInfo.pos.x < Me.Width - 10 Then
+            e.Graphics.DrawImage(playerBitmap(plrInfo.anim), plrInfo.pos.x, plrInfo.pos.y, playerBitmap(plrInfo.anim).Width, playerBitmap(plrInfo.anim).Height)
+        ElseIf isEnd = False Then
+            isEnd = True
+            MsgBox("열심히 달려 마을에 도착했습니다." & vbCrLf & vbCrLf & "확인 버튼을 누르면 다음 스테이지로 이동합니다",, "NEXT STAGE")
+            thread_main.Abort()
+            Form1.Show()
+            Me.Close()
+        End If
+        If isStart = True Then
+            e.Graphics.FillRectangle(brush1, 20, 20, 10, 20)
+            e.Graphics.FillRectangle(brush2, 30, 20, timeGauge, 20)
+
+            e.Graphics.FillRectangle(brush1, Me.Width - 40, 20, 10, 20)
+
+            If timeGauge < Me.Width - 70 Then
+                timeGauge = CInt((Me.Width - 70) / 5) * CInt((currentTime - startTime) / 1000)  '600 / 200으로 바꾸자
+            End If
+
+            If timeGauge >= Me.Width - 70 Then
+                timeGauge = Me.Width - 70
+            End If
+        End If
+
+            If isJumpEffectSound = True And isJump = True Then
+            isJumpEffectSound = False
+            mciSendString("play jumpsound1 from 0", 0, 0, 0)
+        End If
     End Sub
     Private Sub Form4_Closed(sender As Object, e As EventArgs) Handles MyBase.Closed
         thread_main.Abort()
+    End Sub
+
+    Private Sub Button1_MouseDown(sender As Object, e As MouseEventArgs) Handles Button1.MouseDown
+        Button1.BackgroundImage = My.Resources.ResourceManager.GetObject("button21")
+    End Sub
+
+    Private Sub Button1_MouseUp(sender As Object, e As MouseEventArgs) Handles Button1.MouseUp
+        thread_main.Abort()
+        Form2.Show()
+        Me.Close()
     End Sub
 End Class
